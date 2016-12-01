ced8b0205be7d33d2e576cab259bf943d1abb6fa
linbin823 <13918814219@163.com>
2016-12-01 16:54:08 +0800

    修改 1、add

---
+++ src/iusrauthorize.h
@@ -0,0 +1,25 @@
+﻿#ifndef IUSRAUTHRIZE
+#define IUSRAUTHRIZE
+#include <QObject>
+
+class IUsrAuthorize
+{
+public:
+    explicit IUsrAuthorize(){}
+    virtual ~IUsrAuthorize(){}
+    //登录
+    virtual bool logIn(const QString& name, const QString& password)=0;
+    //登出
+    virtual bool logOut( void )=0;
+    //设置自动登出功能
+    virtual bool setAutoLogOut(bool en, int durationSec)=0;
+    //查询自动登出功能
+    virtual void getAutoLogOut(bool* en, int* durationSec) const =0;
+    //查询用户名和级别
+    virtual bool getNameAndLevel(QString *name, quint8 *level ) const=0;
+
+};
+
+
+#endif // IUSRAUTHRIZE
+
---
+++ src/iusrmanage.h
@@ -0,0 +1,20 @@
+﻿#ifndef IUSRMANAGE
+#define IUSRMANAGE
+#include <QObject>
+#include <QList>
+#include "usrinfo.h"
+
+class IUsrManage
+{
+public:
+    explicit IUsrManage(){}
+    virtual ~IUsrManage(){}
+    //添加用户
+    virtual bool addUsr(const QString& name, const QString& pswd, const quint8& level) = 0;
+    //删除用户
+    virtual bool deleteUsr(const QString& name) = 0;
+    //获得用户列表
+    virtual QList<UsrInfo*> getUsrList() = 0;
+};
+#endif // IUSRMANAGE
+
